7e194127c2edbcc9646fb8328a874038f1beb5ef
mark-zalewski <64677761+mark-zalewski@users.noreply.github.com>
2025-07-26 08:47:02 -0500

    Update k3s.sh

--- Kubernetes/K3S-Deploy/k3s.sh
+++ Kubernetes/K3S-Deploy/k3s.sh
@@ -61,7 +61,7 @@ lbrange=10.0.10.20-10.0.10.80
 certName=id_ed25519
 
 #ssh config file
-config_file=~/.ssh
+config_file=~/.ssh/config
 
 #############################################
 #            DO NOT EDIT BELOW              #
